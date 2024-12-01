91742df1f77960af1052eb9676c182040e9b2904
AkashJana18 <akashjana663@gmail.com>
2024-12-01 18:16:57 +0530

    adding logo

--- src/components/CustomSidebar.tsx
+++ src/components/CustomSidebar.tsx
@@ -107,7 +107,7 @@ export const Logo = () => {
       href="/"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
-      <div className="h-5 w-6 bg-gradient-to-r from-cyan-300 to-blue-500 rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <img src="/gs.png" alt="GS" width={32} />
       <motion.span
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -125,7 +125,7 @@ export const LogoIcon = () => {
       href="/"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
-      <div className="h-5 w-6 bg-gradient-to-r from-cyan-300 to-blue-500 rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <img src="/gs.png" alt="GS" width={40}/>
     </Link>
   );
 };
